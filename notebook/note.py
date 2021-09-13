eb3f20696d4179442d60820afd1ddfe99ea3fce7
EvenPan <yz830620@gmail.com>
2021-09-13 23:38:18 +0800

    first commit

---
+++ notebook/note.py
@@ -0,0 +1,19 @@
+"""class notebook"""
+
+class Notebook:
+    note: list = []
+    def __init__(self):
+        pass
+    
+    def search(filter: str):
+        pass
+
+    def new_note(note_id, tag=""):
+        pass
+
+    def modify_memo(note_id, memo):
+        pass
+
+    def modify_tag(note_id, tag):
+        pass
+
