7fb57e6c765e8a760d2af0142e4213cf2c17267a
Even Pan <evenpan@fareastone.com.tw>
2021-10-22 11:10:53 +0800

    reliable zmq

--- other/zmq/zmq_test.py
+++ other/zmq/zmq_test.py
@@ -1,9 +1,55 @@
+#  Lazy Pirate client
+#  Use zmq_poll to do a safe request-reply
+#  To run, start lpserver and then randomly kill/restart it
+#
+#   Author: Daniel Lundin <dln(at)eintr(dot)org>
+#
+import itertools
+import logging
+import sys
 import zmq
+
+logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
+
+REQUEST_TIMEOUT = 2500
+REQUEST_RETRIES = 3
+SERVER_ENDPOINT = "tcp://localhost:5555"
 
 context = zmq.Context()
 
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://127.0.0.1:1234")
+logging.info("Connecting to server…")
+client = context.socket(zmq.REQ)
+client.connect(SERVER_ENDPOINT)
 
-socket.send_string('hello')
-print(socket.recv())
+for sequence in itertools.count():
+    request = str(sequence).encode()
+    logging.info("Sending (%s)", request)
+    client.send(request)
+
+    retries_left = REQUEST_RETRIES
+    while True:
+        if (client.poll(REQUEST_TIMEOUT) & zmq.POLLIN) != 0:
+            reply = client.recv()
+            if int(reply) == sequence:
+                logging.info("Server replied OK (%s)", reply)
+                retries_left = REQUEST_RETRIES
+                break
+            else:
+                logging.error("Malformed reply from server: %s", reply)
+                continue
+
+        retries_left -= 1
+        logging.warning("No response from server")
+        # Socket is confused. Close and remove it.
+        client.setsockopt(zmq.LINGER, 0)
+        client.close()
+        if retries_left == 0:
+            logging.error("Server seems to be offline, abandoning")
+            sys.exit()
+
+        logging.info("Reconnecting to server…")
+        # Create new connection
+        client = context.socket(zmq.REQ)
+        client.connect(SERVER_ENDPOINT)
+        logging.info("Resending (%s)", request)
+        client.send(request)
